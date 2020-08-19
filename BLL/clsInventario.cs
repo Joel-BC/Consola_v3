c29c2653277c46bc362bbe67b7fa722d56070831
Usuario <Usuario@DESKTOP-GE5HRHG>
2020-08-19 14:20:42 -0800

    Agregar archivos de proyecto.

---
+++ BLL/clsInventario.cs
@@ -0,0 +1,143 @@
+﻿using DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+    public class clsInventario
+    {
+        public List<ConsultarInventarioResult> ConsultarInventario()
+        {
+            try
+            {
+                DatosDataContext dc = new DatosDataContext();
+                List<ConsultarInventarioResult> data = dc.ConsultarInventario().ToList();
+                return data;
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        public List<ConsultaInventarioResult> ConsultaInventario(int Codigo)
+        {
+            try
+            {
+                DatosDataContext dc = new DatosDataContext();
+                List<ConsultaInventarioResult> data = dc.ConsultaInventario(Codigo).ToList();
+                return data;
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+
+        public List<ConsultarIdProveedorResult> ConsultarIdProveedor()
+        {
+            try
+            {
+                DatosDataContext dc = new DatosDataContext();
+                List<ConsultarIdProveedorResult> data = dc.ConsultarIdProveedor().ToList();
+                return data;
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        public int ConsultaIdBodega(string codigoBodega)
+        {
+            try
+            {
+                DatosDataContext dc = new DatosDataContext();
+                int ID = dc.ConsultaIdBodega(codigoBodega);
+                return ID;
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        public List<ConsultarNombreBodegaResult> ConsultarNombreBodega()
+        {
+            try
+            {
+                DatosDataContext dc = new DatosDataContext();
+                List<ConsultarNombreBodegaResult> data = dc.ConsultarNombreBodega().ToList();
+                return data;
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        public bool AgregarInventario(string codigo, string nombreProducto, int unidad, int idBodega, int idProveedor, bool estadoStock)
+        {
+            try
+            {
+                int respuesta = 1;
+                DatosDataContext dc = new DatosDataContext();
+                respuesta = Convert.ToInt32(dc.AgregarInventario(codigo, nombreProducto, unidad, idBodega, idProveedor, estadoStock));
+
+                if (respuesta == 0)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        //public bool ActualizarStock(int idStock, string codigo, string nombreProducto, int unidad, int idBodega
+        //    , int idProveedor, bool estadoStock)
+        //{
+        //    try
+        //    {
+        //        int respuesta = 1;
+        //        DatosDataContext dc = new DatosDataContext();
+        //        respuesta = dc.ActualizarStock(idStock, codigo, nombreProducto, unidad, idBodega, idProveedor, estadoStock);
+        //        if (respuesta == 0)
+        //        {
+        //            return true;
+        //        }
+        //        else
+        //        {
+        //            return false;
+        //        }
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        return false;
+        //    }
+        //}
+
+        public bool DeshabilitarInventario(int IdStock)
+        {
+            try
+            {
+                DatosDataContext dc = new DatosDataContext();
+                dc.DeshabilitarInventario(IdStock);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Consola/Controllers/InventarioController.cs
@@ -0,0 +1,92 @@
+﻿using BLL;
+using Consola.Helpers;
+using Consola.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Consola.Controllers
+{
+    [SessionManage]
+    public class InventarioController : Controller
+    {
+        // GET: Inventario
+        public ActionResult Index()
+        {
+            try
+            {
+                List<Inventario> listaInventario = new List<Inventario>();
+                clsInventario invent = new clsInventario();
+                var data = invent.ConsultarInventario().ToList();
+
+                foreach (var item in data)
+                {
+                    Inventario modelo = new Inventario();
+                    modelo.idStock = item.idStock;
+                    modelo.codigo = item.codigo;
+                    modelo.nombreProducto = item.nombreProducto;
+                    modelo.unidad = item.unidad;
+                    modelo.idBodega = item.idBodega;
+                    modelo.idProveedor = item.idProveedor;
+                    modelo.estadoStock = item.estadoStock;
+
+                    listaInventario.Add(modelo);
+                }
+                return View(listaInventario);
+            }
+            catch
+            {
+                string descMsg = "Error consultando la informacion del CLiente.";
+                //Bitacora
+                return RedirectToAction("Error", "Error");
+            }
+        }
+
+        // GET: Inventario/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Inventario/Create
+        public ActionResult Crear()
+        {
+            return View();
+        }
+
+        // POST: Inventario/Create
+        [HttpPost]
+        public ActionResult Crear(Inventario stocks)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    clsInventario objstock = new clsInventario();
+
+                    bool Resultado = objstock.AgregarInventario(stocks.codigo, stocks.nombreProducto, stocks.unidad, stocks.idBodega, stocks.idProveedor, true);
+
+                    if (Resultado)
+                    {
+                        return RedirectToAction("Index");
+                    }
+                    else
+                    {
+                        return View("Crear");
+                    }
+                }
+                else
+                {
+                    return View("Crear");
+                }
+            }
+            catch
+            {
+                TempData["errorMensaje"] = "Inserte correctamente el formato de los datos.";
+                return View();
+            }
+        }
+    }
+}
